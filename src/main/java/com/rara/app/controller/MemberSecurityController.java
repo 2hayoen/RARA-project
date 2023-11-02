72ba48f55885f9a5ac28f33c020625f5d6a595f7
sincerelie <wjd00006636@gmail.com>
2023-11-02 20:42:09 +0900

    feat : "Security 적용 및 작동 확인"
    Spring Security 적용하기 위해 의존성 추가 및 관련 Config 등 추가.
    그 외 MemberDTO, MemberService 등 수정.
    이와 관련하여 기능 개선 및 보완 필요.

---
+++ src/main/java/com/rara/app/controller/MemberSecurityController.java
@@ -0,0 +1,129 @@
+package com.rara.app.controller;
+
+import com.rara.app.dto.MemberDTO;
+import com.rara.app.service.MemberService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class MemberSecurityController {
+    @Autowired
+    private MemberService memberService;
+
+    @GetMapping("/personal")
+    public String home(Model model) { // 인증된 사용자의 정보를 보여줌
+
+        MemberDTO member = (MemberDTO) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Long id = member.getId();
+
+        try {
+            MemberDTO memberDTO = memberService.selectMemberById(id);
+            memberDTO.setPwd(memberService.passwordEncoder().encode(memberDTO.getPwd())); // password는 보이지 않도록 null로 설정
+            model.addAttribute("users", memberDTO);
+            return "home1";
+        } catch (Exception e) {
+            model.addAttribute("msg", "Member personal Error!");
+            return "error";
+        }
+
+    }
+
+    @GetMapping("/userList")
+    public String getMembers(Model model) { // User 테이블의 전체 정보를 보여줌
+        try {
+            List<MemberDTO> members = memberService.selectMembersAll();
+            model.addAttribute("list", members);
+            return "userList";
+        } catch (Exception e) {
+            model.addAttribute("msg", "Member MemberList Error!");
+            return "error";
+        }
+
+    }
+
+    @GetMapping("/login")
+    public String loginPage() { // 로그인되지 않은 상태이면 로그인 페이지를, 로그인된 상태이면 home 페이지를 보여줌
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication instanceof AnonymousAuthenticationToken)
+            return "login";
+        return "redirect:/";
+    }
+
+    @GetMapping("/signup")
+    public String signupPage() {  // 회원 가입 페이지
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication instanceof AnonymousAuthenticationToken)
+            return "signup";
+        return "redirect:/";
+    }
+
+    @PostMapping("/signup")
+    public String signup(MemberDTO memberDTO) { // 회원 가입
+        try {
+            memberService.insertMember(memberDTO);
+        } catch (Exception e) {
+//            model.addAttribute("msg", "Member signup Error!");
+            return "error";
+        }
+        return "redirect:/login";
+    }
+
+    @GetMapping("/update")
+    public String editPage(Model model) { // 회원 정보 수정 페이지
+        MemberDTO member = (MemberDTO) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Long id = member.getId();
+
+        try {
+            MemberDTO memberDTO = memberService.selectMemberById(id);
+            model.addAttribute("user", memberDTO);
+            return "editPage";
+        } catch (Exception e) {
+            model.addAttribute("msg", "Member edit page Error!");
+            return "error";
+        }
+
+    }
+
+    @PostMapping("/update")
+    public String edit(MemberDTO memberDTO, Model model) { // 회원 정보 수정
+        MemberDTO member = (MemberDTO) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Long id = member.getId();
+        memberDTO.setId(id);
+        try {
+            memberService.updateMember(memberDTO);
+            return "redirect:/";
+        } catch (Exception e) {
+            model.addAttribute("msg", "Member edit Error!");
+            return "error";
+        }
+
+    }
+
+    @PostMapping("/delete")
+    public String withdraw(HttpSession session, Model model) { // 회원 탈퇴
+        MemberDTO member = (MemberDTO) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Long id = member.getId();
+
+        try {
+            if (id != null) {
+                memberService.deleteMember(id);
+            }
+            SecurityContextHolder.clearContext(); // SecurityContextHolder에 남아있는 token 삭제
+            return "redirect:/logout";
+        } catch (Exception e) {
+            model.addAttribute("msg", "Member delete Error!");
+            return "error";
+        }
+
+    }
+
+}
--- src/main/java/com/rara/app/dto/MemberDTO.java
+++ src/main/java/com/rara/app/dto/MemberDTO.java
@@ -4,13 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class MemberDTO {
+public class MemberDTO implements UserDetails {
     private Long id;
     private String email;
     private String pwd;
@@ -30,4 +36,39 @@ public class MemberDTO {
 
     // 구분자
     private String separator;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("user"));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public String getPassword() {
+        return pwd;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
