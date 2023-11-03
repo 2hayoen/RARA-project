11ff2aede0667cea48e307dcee9716da076d9ba1
hyeon0563 <tjrgus0563@naver.com>
2023-11-03 09:17:59 +0900

    addition webpage

--- src/main/java/com/rara/app/RaraApplication.java
+++ src/main/java/com/rara/app/RaraApplication.java
@@ -7,11 +7,12 @@ import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @MapperScan("com.rara.app.model.mapper")
 public class RaraApplication {
 
