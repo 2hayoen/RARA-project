b07b035efad26f997e86a8fd1c5d49941d818cdc
sincerelie <wjd00006636@gmail.com>
2023-11-03 21:18:34 +0900

    feat : "Security Session과 Thymeleaf 활용 시도 중"
    Controller에서 MemberDTO member = (MemberDTO) SecurityContextHolder.getContext().getAuthentication().getPrincipal();로는 접근이 가능한 Session 정보를 Thymeleaf로는 접근이 불가능하여 해당 문제 해결 시도 중

--- src/main/java/com/rara/app/service/MemberDetailService.java
+++ src/main/java/com/rara/app/service/MemberDetailService.java
@@ -3,6 +3,7 @@ package com.rara.app.service;
 import com.rara.app.dto.MemberDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -12,11 +13,11 @@ public class MemberDetailService implements UserDetailsService {
     private final MemberService memberService;
 
     @Override
-    public MemberDTO loadUserByUsername(String email) {
+    public MemberDTO loadUserByUsername(String email) throws UsernameNotFoundException {
         try {
             return memberService.selectMemberByEmail(email);
         } catch (Exception e) {
-            return null;
+            throw new UsernameNotFoundException("User not found with email: " + email);
         }
     }
 }
