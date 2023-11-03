69d5674a7e98b7c831ad8f620753233443610e0c
hyeon0563 <tjrgus0563@naver.com>
2023-11-03 15:40:50 +0900

    haha hoho

--- src/main/java/com/rara/app/controller/WebTestController.java
+++ src/main/java/com/rara/app/controller/WebTestController.java
@@ -1,73 +1,73 @@
-//package com.rara.app.controller;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//
-//@Controller
-//public class WebTestController {
-//
-//    @GetMapping("/base_test2")
-//    public String base_test2(Model model) {
-//        return "base2";
-//    }
-//
-//    @GetMapping("/")
-//    public String main_home(Model model) {
-//        return "index";
-//    }
-//
-//    @GetMapping("/base_test")
-//    public String base_test(Model model) {
-//        return "base_test";
-//    }
-//
-//
-//    @GetMapping("/classchild")
-//    public String child_class(Model model) {
-//        return "classchild";
-//    }
-//
-//    @GetMapping("/notice_create")
-//    public String notice_create(Model model) {
-//        return "notice_create";
-//    }
-//
-//    @GetMapping("/mypage")
-//    public String mypage(Model model) {
-//        return "mypage";
-//    }
-//
-//    @GetMapping("/teacher")
-//    public String teacher_page(Model model) {
-//        return "teacher_page";
-//    }
-//
-//    @GetMapping("/parents")
-//    public String parents_page(Model model) {
-//        return "parents_page";
-//    }
-//
-//    @GetMapping("/schedule")
-//    public String schedule(Model model) {
-//        return "schedule";
-//    }
-//
+package com.rara.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebTestController {
+
+    @GetMapping("/base_test2")
+    public String base_test2(Model model) {
+        return "base2";
+    }
+
+    @GetMapping("/")
+    public String main_home(Model model) {
+        return "index";
+    }
+
+    @GetMapping("/base_test")
+    public String base_test(Model model) {
+        return "base_test";
+    }
+
+
+    @GetMapping("/classchild")
+    public String child_class(Model model) {
+        return "classchild";
+    }
+
+    @GetMapping("/notice_create")
+    public String notice_create(Model model) {
+        return "notice_create";
+    }
+
+    @GetMapping("/mypage")
+    public String mypage(Model model) {
+        return "mypage";
+    }
+
+    @GetMapping("/teacher")
+    public String teacher_page(Model model) {
+        return "teacher_page";
+    }
+
+    @GetMapping("/parents")
+    public String parents_page(Model model) {
+        return "parents_page";
+    }
+
+    @GetMapping("/schedule")
+    public String schedule(Model model) {
+        return "schedule";
+    }
+
 //    @GetMapping("/signup")
 //    public String signup(Model model) {
 //        return "signup";
 //    }
-//
+
 //    @GetMapping("/signin")
 //    public String signin(Model model) {
 //        return "signin";
 //    }
-//
-//
-//
-//    @GetMapping("/writingboard")
-//    public String writingboard(Model model) {
-//        return "writingboard";
-//    }
-//
-//}
+
+
+
+    @GetMapping("/writingboard")
+    public String writingboard(Model model) {
+        return "writingboard";
+    }
+
+}
